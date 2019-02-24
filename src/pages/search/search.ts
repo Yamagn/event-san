988c008610f35f0223fd79fe0b1440e2acaef176
Yamagn <syankenpon@gmail.com>
2019-02-25 00:42:12 +0900

    add search bar

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -15,11 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SearchPage {
 
+  keywords: string = "";
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
+  }
+
+  getEvents(ev) {
+    console.log(this.keywords);
   }
 
 }
